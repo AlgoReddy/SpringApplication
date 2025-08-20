214049949ba4743d1ed4ec6b6f7536277289ff76
algoreddy <helloyash99@gmail.com>
2025-08-20 16:03:57 +0530

    initialized  the git repository for spring applicattion

---
+++ src/main/java/com/springapplication/buildingbackendwithspring/BuildingbackendwithspringApplication.java
@@ -0,0 +1,13 @@
+package com.springapplication.buildingbackendwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingbackendwithspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingbackendwithspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springapplication/buildingbackendwithspring/BuildingbackendwithspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapplication.buildingbackendwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingbackendwithspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
